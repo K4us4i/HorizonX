9ed862316976786ae793fbd7dd95a167cb998987
Khushi Paudel Khatri <khatripaudel0@gmail.com>
2025-12-06 15:00:52 +0545

    api

--- localhelp.py
+++ localhelp.py
@@ -1,34 +1,31 @@
 # Local Community Help
 # Community Connectivity
-from flask import Flask, render_template, request
-import requests # type: ignore
+from flask import Flask, render_template, request, jsonify
+import requests 
 
 app = Flask(__name__)
 
 @app.route("/events")
 def get_events():
-    url = "https://calendar.bloggernepal.com/api/today"
+     url = "https://date.nager.at/api/v3/PublicHolidays/2025/AT"
     
-    headers = {
-        "Authorization": "Bearer YOUR_EVENTBRITE_TOKEN"
+     headers = {"Authorization": "Bearer YOUR_EVENTBRITE_TOKEN"
     }
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    # return jsonify(data)
+     response = requests.get(url, headers=headers)
+     data = response.json()
+     return jsonify(data)
 
 @app.route("/alerts")
 def get_alerts():
-    url = "https://api.weather.gov/alerts/active"
+    url = "https://newsdata.io/api/1/latest?apikey=pub_0862588a38234c4caf78bfb3b8f03d44&q=news"
     response = requests.get(url)
     data = response.json()
-
-    # return jsonify(data)
+    return jsonify(data)
 
 @app.route("/")
 def home():
-    return render_template("page.html")
+    return render_template("pages.html")
 
 
 if __name__ == "__main__":
