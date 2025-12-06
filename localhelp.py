b9d2353e8485413e71ed7c2e61c6084594dfe8a7
Khushi Paudel Khatri <khatripaudel0@gmail.com>
2025-12-06 11:26:56 +0545

    saved changes

--- localhelp.py
+++ localhelp.py
@@ -4,4 +4,4 @@ import requests
 response = requests.get("https://calendar.bloggernepal.com/api/today")
 data=response.json()
 
-
+response=requests.post("https://calendar.bloggernepal.com/api/today", json)
