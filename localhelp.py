c6c66475f4c917a0b79d597658d1d0c8e3b244d8
Khushi Paudel Khatri <khatripaudel0@gmail.com>
2025-12-06 11:57:22 +0545

    api

--- localhelp.py
+++ localhelp.py
@@ -1,7 +1,28 @@
 # Local Community Help
 # Community Connectivity
+from flask import Flask, render_template, request
 import requests
-response = requests.get("https://calendar.bloggernepal.com/api/today")
-data=response.json()
 
-response=requests.post("https://calendar.bloggernepal.com/api/today", json)
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+
+    if request.method == "POST":
+        event = request.form.get("event")
+        url = "https://calendar.bloggernepal.com/api/today"
+
+        headers = {"Content-Type": "application/json"}
+        payload = {"text": event_text}
+
+        response = requests.post(url, json=payload, headers=headers)
+        data = response.json()
+
+        result = data["result"]["type"]
+
+    return render_template("page.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
